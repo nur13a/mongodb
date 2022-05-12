205851cb897ba83cc5c8c07db6f0a70cf8efc520
Жоробаева Нуриза Самидиновна <nuzhorobaeva@megacom.local>
2022-05-12 16:50:27 +0600

    Added few lines of code

--- src/main/java/com/mongo/mongodb/MongodbApplication.java
+++ src/main/java/com/mongo/mongodb/MongodbApplication.java
@@ -17,41 +17,54 @@ import java.util.List;
 @SpringBootApplication
 public class MongodbApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(MongodbApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(MongodbApplication.class, args);
+    }
 
-	@Bean
-	CommandLineRunner runner(StudentRepository repository, MongoTemplate template){
-		return  args -> {
-			Address address = new Address(
-					"Kyrgyzstan",
-					"Bishkek",
-					"312"
-			);
-			ArrayList <String> list = new ArrayList<>();
-			list.add("HELLO");
-			list.add("World");
-			String lastname = "Zhorobaeva12";
-			Student student = new Student(
-					"Nuriza",
-					lastname,
-					Gender.FEMALE,
-					address,
-					list,
-					new BigDecimal(4568),
-					new Date()
-			);
-			Query query = new Query();
-			query.addCriteria(Criteria.where("lastname").is(lastname));
-			List<Student> students=template.find(query, Student.class);
+    @Bean
+    CommandLineRunner runner(StudentRepository repository, MongoTemplate template) {
+        return args -> {
+            Address address = new Address(
+                    "Kyrgyzstan",
+                    "Bishkek",
+                    "312"
+            );
+            ArrayList<String> list = new ArrayList<>();
+            list.add("HELLO");
+            list.add("World");
+            String lastname = "Zhorobaeva12";
+            Student student = new Student(
+                    "Nuriza",
+                    lastname,
+                    Gender.FEMALE,
+                    address,
+                    list,
+                    new BigDecimal(4568),
+                    new Date()
+            );
 
-			if(students.size()>1){
-				throw new IllegalStateException("Repeated lines" + lastname );
-			}
-			if(students.isEmpty())
-				repository.insert(student);
-			else System.out.println("The Stident exist " + student);
-		};
-	}
+            repository.findStudentByLastName(lastname).ifPresentOrElse(s ->
+                    {
+                        System.out.println(student + " already exists");
+                    }, () -> {
+                        System.out.println(student + " already not exists");
+                        repository.insert(student);
+                    }
+            );
+            //extracted(repository, template, lastname, student);
+        };
+    }
+
+    private void extracted(StudentRepository repository, MongoTemplate template, String lastname, Student student) {
+        Query query = new Query();
+        query.addCriteria(Criteria.where("lastname").is(lastname));
+        List<Student> students = template.find(query, Student.class);
+
+        if (students.size() > 1) {
+            throw new IllegalStateException("Repeated lines" + lastname);
+        }
+        if (students.isEmpty())
+            repository.insert(student);
+        else System.out.println("The Stident exist " + student);
+    }
 }
--- src/main/java/com/mongo/mongodb/StudentRepository.java
+++ src/main/java/com/mongo/mongodb/StudentRepository.java
@@ -2,6 +2,9 @@ package com.mongo.mongodb;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
 
+import java.util.Optional;
+
 public interface  StudentRepository extends MongoRepository<Student, String> {
 
+    Optional<Student> findStudentByLastName(String lastname);
 }
